c3c384d96cb11f204d79828bb47f865537a8de98
Carl Rasmusson <carl01rasmusson@gmail.com>
2021-02-15 21:27:53 +0100

    improved cell init and allowed move func

--- board/cell.py
+++ board/cell.py
@@ -1,5 +1,9 @@
-from board.board import Board
+from units.unit import Unit
 
-class Cell(Board):
-    def __init__(self):
-        super(Cell, self).__init__()
+
+class Cell:
+    def __init__(self, x_cord: int, y_cord: int, occupied: bool, current_unit: Unit):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.occupied = occupied
+        self.current_unit = current_unit
--- units/unit.py
+++ units/unit.py
@@ -1,5 +1,4 @@
 
-
 class Unit:
     def __init__(self, unit_features: tuple):
         x, y, state, team = unit_features
@@ -9,10 +8,14 @@ class Unit:
         self.team = team
 
     def allowed_moves(self, move_func):
-        allowed_moves = []
         for cell in board:
-            allowed_moves.append(move_func(cell))
+            if move_func(cell.x_cord, cell.y_cord):
+                if cell.occupied:
+                    occupied_cell_cords = (cell.x_cord, cell.y_cord)
+                    if cell.current_unit.team == self.team:
+                        # not allowed
+                    else:
+                        # allowed
 
-
-
-
+                else:
+                    # allowed
