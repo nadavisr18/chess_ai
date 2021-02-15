0a1aea46679a3cfc79aa96310c2ccd6957753bfb
Carl Rasmusson <carl01rasmusson@gmail.com>
2021-02-15 22:30:03 +0100

    Removed occupied attribute from cell

--- board/cell.py
+++ board/cell.py
@@ -2,8 +2,5 @@ from units.unit import Unit
 
 
 class Cell:
-    def __init__(self, x_cord: int, y_cord: int, occupied: bool, current_unit: Unit):
-        self.x_cord = x_cord
-        self.y_cord = y_cord
-        self.occupied = occupied
+    def __init__(self, current_unit: Unit):
         self.current_unit = current_unit
