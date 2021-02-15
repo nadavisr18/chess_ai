495d1082845c3572e412ba780dd62be5cfc0d49d
Carl Rasmusson <carl01rasmusson@gmail.com>
2021-02-15 22:31:10 +0100

    Started board init, removed pasta code in unit allowed moeve func

--- board/board.py
+++ board/board.py
@@ -1,5 +1,11 @@
-
+from cell import *
 
 class Board:
     def __init__(self):
-        pass
+        self.board = []
+        for i in range(8):
+            l = []
+            for i in range(8):
+                l.append(Cell(None))
+
+            self.board.append(l)
--- units/unit.py
+++ units/unit.py
@@ -8,14 +8,4 @@ class Unit:
         self.team = team
 
     def allowed_moves(self, move_func):
-        for cell in board:
-            if move_func(cell.x_cord, cell.y_cord):
-                if cell.occupied:
-                    occupied_cell_cords = (cell.x_cord, cell.y_cord)
-                    if cell.current_unit.team == self.team:
-                        # not allowed
-                    else:
-                        # allowed
-
-                else:
-                    # allowed
+        pass
