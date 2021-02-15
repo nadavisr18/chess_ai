11a1eb0042ad9855361cef9682df9438363db0ab
Carl Rasmusson <carl01rasmusson@gmail.com>
2021-02-15 22:01:44 +0100

    added move func for bishop

--- units/bishop.py
+++ units/bishop.py
@@ -4,3 +4,11 @@ from units.unit import Unit
 class Bishop(Unit):
     def __init__(self, unit_features: tuple):
         super(Bishop, self).__init__(unit_features)
+
+    def move(self, x: int, y: int):
+        if x == self.x or y == self.y:
+            return False
+        elif abs(x - self.x) == abs(y - self.y):
+            return True
+        else:
+            return False
