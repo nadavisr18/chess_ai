988994ef6c14bc87236423a47cc9eab80c9b3c93
Carl Rasmusson <carl01rasmusson@gmail.com>
2021-02-16 20:26:56 +0100

    Added variabels to controll board size

--- board/board.py
+++ board/board.py
@@ -1,11 +1,13 @@
 from cell import *
 
+# board_row_size and board_column_size needs to be imported from config
+
 class Board:
     def __init__(self):
         self.board = []
-        for i in range(8):
+        for i in range(board_row_size):
             l = []
-            for i in range(8):
+            for i in range(board_column_size):
                 l.append(Cell(None))
 
             self.board.append(l)
